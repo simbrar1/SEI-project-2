b3c946b9a6b802c71ccca9f154f2b5622bd02dac
simbrar1 <sim.brar1@hotmail.com>
2019-06-20 17:23:52 +0100

    answers working

--- src/components/QuizPage.js
+++ src/components/QuizPage.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import axios from 'axios'
 
+import Results from './Results'
+
 
 class QuizPage extends React.Component {
   constructor() {
     super()
 
-    this.state = { quiz: [] }
+    this.state = { quiz: [], counter: 0 }
 
     this.showAnswer = this.showAnswer.bind(this)
     this.nextQuestion = this.nextQuestion.bind(this)
@@ -19,7 +21,6 @@ class QuizPage extends React.Component {
   }
 
   random() {
-    console.log('running')
     const questions = this.state.quiz
     const currentQuestion = questions[Math.floor(Math.random() * questions.length)]
     const answers = [...currentQuestion.incorrect_answers, currentQuestion.correct_answer]
@@ -29,38 +30,56 @@ class QuizPage extends React.Component {
   showAnswer(e) {
     e.preventDefault()
     if (e.target.value === this.state.currentQuestion.correct_answer ) {
-      const right = 'Right'
-      this.setState({ right })
+      const result = 'Right'
+      this.setState({ result })
     } else {
-      const wrong = 'Wrong'
-      this.setState({ wrong })
+      const result = 'Wrong'
+      this.setState({ result })
     }
   }
 
   nextQuestion(e) {
     e.preventDefault()
     this.random()
+    const result = ''
+    this.setState({ result })
+    let counter = this.state.counter
+    counter ++
+    this.setState({ counter })
+  }
+
+  score() {
+    
   }
 
 
   render() {
     if (!this.state.quiz || !this.state.currentQuestion || !this.state.answers) return null
-    const { currentQuestion, answers, right, wrong } = this.state
+    const { currentQuestion, answers, result } = this.state
     return (
       <main>
-        <div>
-          Question:
-          {currentQuestion.question}
-        </div>
-        <div>
-          Answers:
-          {answers.map((answer, i) => (
-            <button onClick={this.showAnswer} key={i} value={answer} type="submit">{answer}
-            </button>
-          ))}
-        </div>
-        <div>{ right }{ wrong }</div>
-        <button onClick={this.nextQuestion}>Next Question</button>
+        {this.state.counter < 10 &&
+          <div>
+            <div>
+              Question:
+              {currentQuestion.question}
+            </div>
+            <div>
+            Answers:
+              {answers.map((answer, i) => (
+                <button onClick={this.showAnswer} key={i} value={answer} type="submit">{answer}
+                </button>
+              ))}
+            </div>
+            <div>{ result }</div>
+            <button onClick={this.nextQuestion}>Next Question</button>
+          </div>
+        }
+        {this.state.counter === 10 &&
+          <Results
+            score={this.state.score}
+          />
+        }
       </main>
     )
   }
---
+++ src/components/Results.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const Results = () => <h1>Results</h1>
+
+export default Results
