b66e181103d0ca172567df8ff096007a42398d0a
simbrar1 <sim.brar1@hotmail.com>
2019-06-20 10:53:43 +0100

    questions showing at rando

--- src/components/QuizPage.js
+++ src/components/QuizPage.js
@@ -6,19 +6,31 @@ class QuizPage extends React.Component {
   constructor() {
     super()
 
-    this.state = { quiz: null }
+    this.state = { quiz: [] }
   }
 
   componentDidMount() {
     axios.get('https://opentdb.com/api.php?amount=50&category=11&type=multiple&difficulty=easy')
-      .then(res => console.log(res.data))
+      .then(res => this.setState({ quiz: res.data.results }))
       .catch(err => console.log(err))
   }
 
+  randomQuestion() {
+    const questions = this.state.quiz
+    return questions[Math.floor(Math.random() * questions.length)]
+  }
+
+
   render() {
+    if (!this.state.quiz.length) return null
+    const currentQuestion = this.randomQuestion()
+    console.log(currentQuestion)
     return (
       <main>
-        <div>Question</div>
+        <div>
+          {currentQuestion.question}
+          {currentQuestion.correct_answer}
+        </div>
         <div>Answers</div>
       </main>
     )
